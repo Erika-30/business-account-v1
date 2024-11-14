3309d69dca13619aa417eb9182a5b5af814e4b28
Erika <huisaj30@gmail.com>
2024-11-14 18:38:11 -0500

    feat: accounts

---
+++ src/main/java/com/bcp/services/account/web/AccountController.java
@@ -0,0 +1,90 @@
+package com.bcp.services.account.web;
+
+import com.bcp.services.account.api.AccountControllerApiDelegate;
+import com.bcp.services.account.business.AccountService;
+import com.bcp.services.account.model.AccountRequest;
+import com.bcp.services.account.model.AccountResponse;
+import com.bcp.services.account.model.CreateAccountResponse;
+import com.bcp.services.account.model.DepositRequest;
+import com.bcp.services.account.model.WithdrawRequest;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Product Controller.
+ * This class is used to handle the product requests.
+ */
+@RequiredArgsConstructor
+@Component
+public class AccountController implements AccountControllerApiDelegate {
+
+  private final AccountService accountService;
+
+  @Override
+  public ResponseEntity<Void> accountsAccountIdDepositPut(String requestId,
+                                                           String accountId,
+                                                          DepositRequest request) {
+    try {
+      accountService.deposit(accountId, request);
+      return ResponseEntity.noContent().build();
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.notFound().build();
+    }
+
+  }
+  
+  @Override
+  public ResponseEntity<Void> accountsAccountIdWithdrawPut(String requestId,
+                                                            String accountId,
+                                                           WithdrawRequest request) {
+    try {
+      accountService.withdraw(accountId, request);
+      return ResponseEntity.noContent().build();
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @Override
+  public ResponseEntity<List<AccountResponse>> accountsGet(String requestId) {
+
+    return Optional.of(ResponseEntity.ok(accountService.findAll()))
+        .orElse(ResponseEntity.notFound().build());
+
+  }
+
+  @Override
+  public ResponseEntity<Void> accountsIdDelete(String requestId,
+                                                String id) {
+    return accountService.deleteById(Long.valueOf(id))
+        ? ResponseEntity.noContent().build()
+        : ResponseEntity.notFound().build();
+
+  }
+
+  @Override
+  public ResponseEntity<AccountResponse> accountsIdGet(String requestId,
+                                                        String id) {
+
+    return accountService.findById(Long.valueOf(id))
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+
+  }
+
+  @Override
+  public ResponseEntity<CreateAccountResponse> accountsPost(String requestId,
+                                                             AccountRequest accountRequest) {
+
+    return Optional.of(ResponseEntity.ok(new CreateAccountResponse()
+                    .id(accountService.save(accountRequest).getId().toString())))
+        .orElse(ResponseEntity.notFound().build());
+
+  }
+
+}
